a3faa3d7f24b63797f623aa9f3c17d2fe0bd224f
gitwakhare <gitwakhare1829@gmail.com>
2023-07-19 20:38:23 +0530

    [gitwakhare] add new project

---
+++ src/test/java/test/mmvan.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class mmvan {
+  @Test
+  public void mav() throws InterruptedException {
+	  WebDriver driver= new ChromeDriver();
+	  Thread.sleep(1000); 
+	  driver.get("https://mvnrepository.com/artifact/org.apache.poi/poi-ooxml/5.2.3");
+driver.close();
+  }
+}
---
+++ src/test/java/test/velocity.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class velocity {
+  @Test
+  public void velc() throws InterruptedException {
+	  
+	  WebDriver driver= new ChromeDriver();
+	  Thread.sleep(1000);
+	  driver.get("https://vctcpune.com/");
+	  
+  }
+}
